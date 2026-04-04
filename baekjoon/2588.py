bca0758a1d651722967cde2d4ebb917f3c043837
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-04 23:49:31 +0900

    solve multiplication process

--- baekjoon/2588.py
+++ baekjoon/2588.py
@@ -3,7 +3,15 @@
 
 A = int(input())
 B = int(input())
+
+# B의 일의 자리와 A의 곱
 print(A * (B % 10))
+
+# B의 십의 자리와 A의 곱
 print(A * ((B // 10) % 10))
+
+# B의 백의 자리와 A의 곱
 print(A * (B // 100))
+
+# A와 B의 전체 곱
 print(A * B)
