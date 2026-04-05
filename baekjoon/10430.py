60409fb2f6795def6dd5684fa8a434a8532f00c3
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-06 02:06:12 +0900

    solve modular arithmetic

--- baekjoon/10430.py
+++ baekjoon/10430.py
@@ -1,8 +1,14 @@
 # problem: 10430
 # tier: bronze
 
-A, B, C, = map(int, input().split())
+# 세 정수 A, B, C 입력 및 변환
+A, B, C = map(int, input().split())
+
+# (A+B)%C 계산 및 출력
 print((A + B) % C)
+# ((A%C) + (B%C))%C 계산 및 출력
 print(((A % C) + (B % C)) % C)
+# (A*B)%C 계산 및 출력
 print((A * B) % C)
+# ((A%C) * (B%C))%C 계산 및 출력
 print(((A % C) * (B % C)) % C)
