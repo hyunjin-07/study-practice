41b3d08ccec2393133be1198e875ae88fd3423cd
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-17 20:08:51 +0900

    star pattern 2

--- baekjoon/2439.py
+++ baekjoon/2439.py
@@ -1,8 +1,10 @@
 # problem: 2439
 # tier: bronze
 
-A = int(input())
+# 별의 개수(N) 입력
+N = int(input())
 
-for i in range(1, A + 1):
-    A -= 1
-    print((" ") * A + ("*") * i)
+# 1부터 N까지 반복하며 별 찍기
+for i in range(1, N + 1):
+    # 공백은 (N-i)개, 별은 i개 출력하여 오른쪽 정렬 구현
+    print(" " * (N - i) + "*" * i)
